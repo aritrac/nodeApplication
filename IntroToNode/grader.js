c40dd5cb7be4e14a771076714b2c1185d195a98e
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-09 09:38:49 +0000

    modified grader.js

--- IntroToNode/grader.js
+++ IntroToNode/grader.js
@@ -7,9 +7,10 @@ function average(arr){
     //Divide and round to nearest integer and return
     return Math.round(total/arr.length);
 }
-
+console.log("Average score for environment science");
 var scores = [90,98,89,100,100,86,94];
 console.log(average(scores)); //should return 94
 
+console.log("Average score for organic chemistry");
 var scores2 = [40,65,77,82,80,54,73,63,95,49];
 console.log(average(scores2)); //should return 68
