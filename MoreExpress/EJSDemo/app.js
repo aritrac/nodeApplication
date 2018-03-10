1c7ca9dd548ec280eaf7652bef8baa05b0a3366f
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-10 05:24:21 +0000

    Added partials templating examples

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,13 +1,19 @@
 var express = require("express");
 var app = express();
 
+/*serving the CSS file which is in this folder*/
+app.use('/public',express.static('public'));
+
+/*We dont have to supply view file extensions to be ejs anymore after this*/
+app.set("view engine","ejs");
+
 app.get("/",function(req,res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req,res){
    var thing = req.params.thing;
-   res.render("love.ejs", {thingVar: thing});
+   res.render("love", {thingVar: thing});
 });
 
 app.get("/posts",function(req,res){
@@ -17,7 +23,7 @@ app.get("/posts",function(req,res){
       {title: "Can you believe this pomsky?", author: "Aritra"}
    ];
    
-   res.render("posts.ejs", {posts: posts});
+   res.render("posts", {posts: posts});
 });
 
 app.listen(process.env.PORT,process.env.IP,function(){
