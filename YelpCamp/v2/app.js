1831343bee52b86f2426502528c3f21a6d94a559
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-12 16:02:30 +0000

    Added the more info page logic

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -11,7 +11,8 @@ app.set("view engine","ejs");
 // Schema Setup
 var campgroundSchema = new mongoose.Schema({
    name: String,
-   image: String
+   image: String,
+   description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -19,7 +20,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 //     {
 //         name: "The Indus River Camp", 
-//         image: "https://pikwizard.com/photos/886ec82a538a0584410f482fd01f0ded-s.jpg"
+//         image: "https://pikwizard.com/photos/886ec82a538a0584410f482fd01f0ded-s.jpg",
+//         description: "This is situated on the picturesque location of Punjab with mother nature at its best. A sight to behold!"
         
 //     },function(err, campground){
 //         if(err){
@@ -34,24 +36,27 @@ app.get("/",function(req,res){
     res.render("landing");
 });
 
+//INDEX ROUTE - show all campgrounds
 app.get("/campgrounds",function(req,res){
     //Get all campgrounds from DB
     Campground.find({},function(err,allCampgrounds){
        if(err){
            console.log(err);
        }else{
-           res.render("campgrounds",{campgrounds : allCampgrounds});
+           res.render("index",{campgrounds : allCampgrounds});
        }
     });
 });
 
+//CREATE - add new campgrounds to database
 app.post("/campgrounds", function(req,res){
     //get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
+    var description = req.body.description;
     var newCampground = {
-        name: name, image: image
-    }
+        name: name, image: image, description: description
+    };
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err,campground){
         if(err){
@@ -63,8 +68,22 @@ app.post("/campgrounds", function(req,res){
     });
 });
 
+//NEW - show form to create new campgrounds
 app.get("/campgrounds/new", function(req,res){
     res.render("new.ejs");
+});
+
+//SHOW - shows more info about one campground
+app.get("/campgrounds/:id",function(req,res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+            //render show template with that campground
+            res.render("show", {campground : foundCampground});
+        }
+    });
 });
 
 app.listen(process.env.PORT,process.env.IP,function(){
--- YelpCamp/v2/readme.md
+++ YelpCamp/v2/readme.md
@@ -17,3 +17,10 @@
 * Show db.collection.drop()  
 * Add a show route/template  
 
+RESTFUL ROUTES  
+name        url         verb            description  
+=======================================================================  
+INDEX       /dogs       GET             Display a list of all dogs  
+NEW         /dogs/new   GET             Displays form to make a new dogs  
+CREATE      /dogs       POST            Add new dog to DB  
+SHOW        /dogs/:id   GET             Shows info about one dog
