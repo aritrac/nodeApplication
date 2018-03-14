1343defa03c535771bc6aa4a31e8a2371742941d
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-14 16:08:56 +0000

    Added new and create route

--- RESTful Routing/BlogSolution/app.js
+++ RESTful Routing/BlogSolution/app.js
@@ -26,6 +26,24 @@ app.get("/", function(req, res){
     res.redirect("/blogs");
 })
 
+//NEW ROUTE
+app.get("/blogs/new", function(req,res){
+    res.render("new");
+});
+//CREATE ROUTE
+app.post("/blogs", function(req,res){
+    //create blog
+    Blog.create(req.body.blog,function(err,newBlog){
+        if(err){
+            res.render("new");
+        }else{
+            //then, redirect to the index
+            res.redirect("/blogs");
+        }
+    });
+    
+});
+
 app.get("/blogs",function(req, res){
     Blog.find({}, function(err,blogs){
         if(err){
--- RESTful Routing/BlogSolution/readme.md
+++ RESTful Routing/BlogSolution/readme.md
@@ -14,3 +14,9 @@
 * Include Semantic UI  
 * Add Simple Nav  
 
+#Putting the C in CRUD  
+* Add NEW route  
+* Add NEW template  
+* Add CREATE route  
+* Add CREATE template  
+
