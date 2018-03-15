66d8e6106eff7b465568786cf2f83f65977136ce
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-15 15:36:51 +0000

    integration passport with mongodb

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -10,6 +10,7 @@ var User                    = require("./models/user");
 mongoose.connect("mongodb://localhost/auth_demo_app");
 var app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(expressSession({
     secret: "GTA-V is better than JustCause",
@@ -23,12 +24,36 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// =============
+// ROUTES
+// =============
+
 app.get("/",function(req,res){
     res.render("home");
 });
 
 app.get("/secret", function(req, res){
     res.render("secret");
+});
+
+// AUTH ROUTES
+// Show Signup Form
+app.get("/register", function(req,res){
+    res.render("register");
+});
+
+//handling user sign up
+app.post("/register", function(req,res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err,user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secret");
+            });
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
