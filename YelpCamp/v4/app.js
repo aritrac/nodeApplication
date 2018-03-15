bb0456ba76226f5d7dc7308f01625f37caad88eb
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-15 11:22:54 +0000

    Added new and create routes for comments

--- YelpCamp/v4/app.js
+++ YelpCamp/v4/app.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 var mongoose   = require("mongoose");
 var app        = express();
 var Campground = require("./models/campground");
+var Comment    = require("./models/comment");
 var seedDB     = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp_v3");
@@ -74,6 +75,7 @@ app.get("/campgrounds/:id",function(req,res){
 // COMMENT ROUTES
 // ================================
 
+// NEW ROUTE
 app.get("/campgrounds/:id/comments/new", function(req,res){
     // find campground by id
     Campground.findById(req.params.id, function(err, campground){
@@ -84,6 +86,30 @@ app.get("/campgrounds/:id/comments/new", function(req,res){
         }
     })
 });
+
+// CREATE ROUTE
+app.post("/campgrounds/:id/comments", function(req, res){
+    //lookup campground using id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }else{
+            Comment.create(req.body.comment, function(err,comment){
+                if(err){
+                    console.log(err);
+                }else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+    //create new comment
+    //connect new comment to campground
+    //redirect to campground show page
+})
 
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("YelpCamp is up");
