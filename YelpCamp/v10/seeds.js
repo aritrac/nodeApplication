9fafe6c07c146829995f9adf319ca398633be74c
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-17 11:36:18 +0000

    Added edit route to campground data

---
+++ YelpCamp/v10/seeds.js
@@ -0,0 +1,66 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var data = [
+    {
+        name: "Cloud's Rest", 
+        image: "https://pikwizard.com/photos/378c44ed3474b70c1a6cc2b034ab40cb-s.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Desert Mesa", 
+        image: "https://pikwizard.com/photos/9087f096b9b81662b96040816979d87a-s.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Canyon Floor", 
+        image: "https://pikwizard.com/photos/4c3595f3ea1f10739068553c0bc2a372-s.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Canyon Floor", 
+        image: "https://pikwizard.com/photos/23b9364b0bc248e44e319e4de5116fb6-s.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+];
+
+function seedDB(){
+    //Remove all campgrounds
+    Campground.remove({},function(err){
+        // if(err){
+        //     console.log(err);
+        // }else{
+        //     console.log("Removed campgrounds!");
+        //     //add a few campgrounds
+        //     data.forEach(function(seed){
+        //         Campground.create(seed, function(err,campground){
+        //             if(err){
+        //                 console.log(err);
+        //             }else{
+        //                 console.log("added a campground!");
+        //                 //create a comment
+        //                 Comment.create(
+        //                     {
+        //                         text: "This place is great, but I wish there was internet",
+        //                         author: "Homer"
+        //                     },function(err,comment){
+        //                         if(err){
+        //                             console.log(err);
+        //                         }else{
+        //                             campground.comments.push(comment);
+        //                             campground.save();
+        //                             console.log("Created new comment");
+        //                         }
+        //                     });
+        //             }
+        //         });    
+        //     });
+        // }
+    });
+    
+    
+    //add a few comments
+}
+
+module.exports = seedDB;
