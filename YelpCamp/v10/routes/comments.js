19aea027344f2d2008a6e23bd08042faaaa0832c
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-03-17 15:02:44 +0000

    Added delete route for comments

--- YelpCamp/v10/routes/comments.js
+++ YelpCamp/v10/routes/comments.js
@@ -50,7 +50,7 @@ router.post("/",isLoggedIn ,function(req, res){
 });
 
 // COMMENTS EDIT ROUTE
-router.get("/:comment_id/edit",function(req,res){
+router.get("/:comment_id/edit",checkCommentOwnership,function(req,res){
     Comment.findById(req.params.comment_id, function(err,foundComment){
         if(err){
             res.redirect("back");
@@ -62,7 +62,7 @@ router.get("/:comment_id/edit",function(req,res){
 
 // COMMENTS UPDATE ROUTE  
 
-router.put("/:comment_id",function(req,res){
+router.put("/:comment_id",checkCommentOwnership,function(req,res){
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err,updatedComment){
         if(err){
             res.redirect("back");
@@ -73,7 +73,7 @@ router.put("/:comment_id",function(req,res){
 });
 
 // COMMENTS DESTROY ROUTE  
-router.delete("/:comment_id", function(req,res){
+router.delete("/:comment_id",checkCommentOwnership, function(req,res){
     //findByIdAndRemove
     Comment.findByIdAndRemove(req.params.comment_id,function(err){
         if(err){
@@ -84,12 +84,33 @@ router.delete("/:comment_id", function(req,res){
     });
 })
 
-// MIDDLEWARE
+// MIDDLEWARES
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
         return next();
     }
     res.redirect("/login");
 }
+
+function checkCommentOwnership(req,res,next){
+    if(req.isAuthenticated()){
+        //does the user own the campground?
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back");
+            }else{
+                //does the user own the comment?
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                }else {
+                    res.redirect("back");
+                }
+            }
+        });
+    }else{
+        res.redirect("back");
+    }
+}
+
 
 module.exports = router;
